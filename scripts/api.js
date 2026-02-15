bd8c1dde08d3ab7688c7f0dac529bdec117da9dd
capolavoropredelli <capolavoro.predelli@gmail.com>
2026-02-15 19:18:24 +0100

    Add files via upload

--- scripts/api.js
+++ scripts/api.js
@@ -1,34 +1,57 @@
-const API_URL = "https://apimusic.predelli.site"
+const API_URL = "http://127.0.0.1:8000"
 
 function get_template_form(meth, form_data) {
     return {
         method: meth,
-        credentiala: "include",
+        credentials: "include",
         body: form_data
     }
 }
 
-async function api_request(route, method, form_data) {
+async function api_request(route, method, form_data, retry = true) {
     const API_ROUTE = API_URL + "/" + route;
     try {
         const response = await fetch(API_ROUTE, get_template_form(method, form_data));
         if (!response.ok) {
-            throw new Error("Response status :" + response.status);
+            const errorData = await response.json();
+            if (errorData.detail.data == "AUTH_ERROR") {
+                if (retry == true) {
+                    const refreshed = await refresh_tokens();
+                    if (refreshed == true) {
+                        return api_request(route, method, form_data, false)
+                    }
+                } else {
+                    window.location.replace("login.html");
+                }
+            }
         }
 
         const result = await response.json();
-
-        if (!res.ok) {
-            throw new Error(data.detail || "Errore sconosciuto")
-        }
-
-        if (!res.success) {
-            throw new Error(data.detail || "Errore sconosciuto")
-        }
 
         return result;
     } catch (error) {
         console.error(error.message);
     }
+}
 
+async function refresh_tokens() {
+    const API_ROUTE = API_URL + "/refresh";
+
+    try {
+        const response = await fetch(API_ROUTE, {
+            method: "POST",
+            credentials: "include"
+        });
+
+        if (!response.ok) {
+            window.location.replace("login.html");
+            return false;
+        }
+
+        return true;
+
+    } catch (error) {
+        console.error(error.message);
+        return false;
+    }
 }
